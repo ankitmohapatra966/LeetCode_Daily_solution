5521ad9246f148c4012fc88f76d03fa309136a06
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-05-04 09:44:08 +0530

    first commit

---
+++ 881. Boats to Save People.cpp
@@ -0,0 +1,14 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());       
+        int x=0;
+        for(int l=0, r=people.size()-1;l<=r; r--){
+            x++;
+            if (people[l]<=limit-people[r])
+                l++;          
+        }
+        return x;
+    }
+};
